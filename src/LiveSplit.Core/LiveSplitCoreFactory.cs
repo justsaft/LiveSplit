e9f4183648d32a85710646b0ffa8a5dd62caec3e
JustSaft <69754418+justsaft@users.noreply.github.com>
2025-02-25 21:25:42 +0100

    use libdl.so.2 on Linux in CoreFactory

--- src/LiveSplit.Core/LiveSplitCoreFactory.cs
+++ src/LiveSplit.Core/LiveSplitCoreFactory.cs
@@ -1,56 +1,95 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
+using System.IO;
 using System.Runtime.InteropServices;
 
 namespace LiveSplit;
 
 public class LiveSplitCoreFactory
 {
-    [DllImport("kernel32")]
-    private unsafe static extern void* LoadLibrary(string dllname);
+    [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Ansi)]
+    private unsafe static extern IntPtr LoadLibrary(string dllname);
 
-    [DllImport("kernel32")]
-    private unsafe static extern void FreeLibrary(void* handle);
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private unsafe static extern bool FreeLibrary(IntPtr handle);
+
+    [DllImport("libdl.so.2")]
+    private unsafe static extern IntPtr dlopen(string filename, int flags);
+
+    /* [DllImport("libdl.so.2")]
+    private unsafe static extern void dlsym(IntPtr handle, string symbol); */
+
+    [DllImport("libdl.so.2")]
+    private unsafe static extern void dlclose(IntPtr handle);
 
     private sealed unsafe class LibraryUnloader
     {
-        internal LibraryUnloader(void* handle)
+        internal LibraryUnloader(IntPtr handle)
         {
-            this.handle = handle;
+            _handle = handle;
         }
 
         ~LibraryUnloader()
         {
-            if (handle != null)
+            if (_handle != null)
             {
-                FreeLibrary(handle);
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    FreeLibrary(_handle);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    dlclose(_handle);
+                }
+                else
+                {
+                    throw new PlatformNotSupportedException("This platform is not supported.");
+                }
             }
         }
 
-        private readonly void* handle;
-
+        private readonly IntPtr _handle;
     }
+
     public static void LoadLiveSplitCore()
     {
         string path;
 
-        if (Unsafe.SizeOf<IntPtr>() == 4)
+        if (Environment.Is64BitOperatingSystem)
         {
-            path = "x86\\livesplit_core.dll";
+            path = Path.Combine(Directory.GetCurrentDirectory(), @"x64/livesplit_core.dll");
         }
         else
         {
-            path = "x64\\livesplit_core.dll";
+            path = Path.Combine(Directory.GetCurrentDirectory(), @"x86/livesplit_core.dll");
         }
 
-        unsafe
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
-            void* handle = LoadLibrary(path);
-
-            if (handle == null)
+            unsafe
             {
-                throw new DllNotFoundException("Unable to load the native livesplit-core library: " + path);
+                IntPtr handle = LoadLibrary(path);
+
+                if (handle == null)
+                {
+                    throw new DllNotFoundException("Unable to load the native livesplit-core library: " + path);
+                }
             }
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            unsafe
+            {
+                IntPtr handle = dlopen(path, 0);
+
+                if (handle == null)
+                {
+                    throw new DllNotFoundException("Unable to load the native livesplit-core library: " + path);
+                }
+            }
+        }
+        else
+        {
+            throw new PlatformNotSupportedException("This platform is not supported.");
         }
     }
 }
